1f188dfb5a524c193f3c6b3ba58e4e5bc3407d68
Samuelsnd <samueldasilva9211@gmail.com>
2023-09-30 00:15:09 -0300

    Content Initial

---
+++ src/main/java/com/crud/projeto/Faculdade/ProjetoFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.crud.projeto.Faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/projeto/Faculdade/ProjetoFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.projeto.Faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
